985bbba2ec2060329ba00e15d6db3a74504d789f
sergeevdms <sergeevdms@gmail.com>
2018-10-15 23:25:06 +0300

    added private route

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
 
@@ -9,4 +10,8 @@ import books from './data/books';
 import 'material-components-web/dist/material-components-web.css';
 import './index.css';
 
-ReactDOM.render(<App topics={topics} books={books} />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <App topics={topics} books={books} />
+  </Router>
+,document.getElementById('root'));
